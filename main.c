a39e33b50988aac963da90a3a34fcd2045bd8128
mishlawi <duartevilar99@gmail.com>
2021-12-10 20:16:13 +0000

    yooo

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 # define N 10
+# define BUCKETS 5
+# define CAP 10
+
+
+struct Bucket {
+int value;
+struct Bucket *prox;
+
+};
+// 
+
+
+
 
 int main(){
 printf("%d",N);
 } 
+
+
