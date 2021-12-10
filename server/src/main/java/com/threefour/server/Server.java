30292799a1c76268ace006dc58f5453eea3055ef
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2021-12-10 10:12:05 +0000

    [server] Prepare server structure

---
+++ server/src/main/java/com/threefour/server/Server.java
@@ -0,0 +1,13 @@
+package com.threefour.server;
+
+public class Server {
+    public static void main(String[] args) {
+        // parse the addresses of neighbors
+
+        // open socket
+
+        // launch thread to send periodic info
+
+        // launch thread to send frames
+    }
+}
---
+++ server/src/main/java/com/threefour/server/worker/Announcer.java
@@ -0,0 +1,11 @@
+package com.threefour.server.worker;
+
+public class Announcer implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
