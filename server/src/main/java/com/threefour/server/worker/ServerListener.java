5d2be0b4867346daefafb39a22ff1de93b5cebcb
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2021-12-20 22:01:21 +0000

    [server] Fix route activation and deactivation

--- server/src/main/java/com/threefour/server/worker/ServerListener.java
+++ server/src/main/java/com/threefour/server/worker/ServerListener.java
@@ -1,5 +1,6 @@
 package com.threefour.server.worker;
 
+import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
@@ -8,12 +9,35 @@ import com.threefour.message.Message;
 import com.threefour.ott.data.RouteTable;
 import com.threefour.ott.worker.Listener;
 import com.threefour.overlay.Neighbours;
+import com.threefour.util.Print;
 
 public class ServerListener extends Listener {
 
     public ServerListener(DatagramSocket socket, Neighbours neighbours) {
         super(socket, neighbours);
         this.routeTable = new RouteTable(null, null, 0);
+    }
+
+    // --------------------
+    // Changed methods
+    // --------------------
+
+    protected void activate(InetAddress address) {
+        if (this.routeTable != null) {
+            // activate child's route
+            var hostname = neighbours.getName(address);
+            this.routeTable.activateRoute(hostname);
+            Print.printInfo("Route to " + hostname + " activated");
+        }
+    }
+
+    protected void deactivate(InetAddress address) {
+        if (this.routeTable != null) {
+            // deactivate child's route
+            var hostname = neighbours.getName(address);
+            this.routeTable.deactivateRoute(hostname);
+            Print.printInfo("Route to " + hostname + " deactivated");
+        }
     }
 
     // --------------------
