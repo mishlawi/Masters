074e776d623a191d9ed67e7bd52537d77f70ae7d
mishlawi <duartevilar99@gmail.com>
2021-12-28 16:57:47 +0000

    Starting to properly organize the work to be done

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/**
+ * @file main.c
+ * @author mishlawi (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
 #include <stdio.h>
 # define N 10
 # define BUCKETS 5
@@ -9,7 +21,6 @@ int value;
 struct Bucket *prox;
 
 };
-// 
 
 
 
