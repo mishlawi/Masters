a7c8fd59225175a872fd5b30ca87b93c62b86c4f
melpereira7 <melanipereira98@gmail.com>
2022-02-28 15:25:36 +0000

    autenticação da chave feita

--- emitter.py
+++ emitter.py
@@ -1,17 +1,42 @@
 import os
-from cryptography.hazmat.primitives import hashes, hmac 
+import cryptography
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import hmac
+import hashlib
 
 class Emitter:
-  def __init__(self, password):
-      self.cipher_key = None
-      self.mac_key = None
-      self.password = password
-      self.key_salt = os.urandom(16)
+    def __init__(self):
+        self.cipher_key = None
+        self.mac_key = None
+        self.key_salt = os.urandom(16)
+        self.nonce = 0
+        self.metadata = 0
       
-  def derivate_key(self):
-      kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=self.key_salt,iterations=390000,)
-      self.mac_key = kdf.derive(self.password)
-    
+    def derivate_key(self):
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=self.key_salt,iterations=390000,)
+        self.mac_key = kdf.derive(b'my great password')
+
+    def send_message(self, message):
+        message = message.encode('utf-8')
+        h = hmac.new(self.mac_key, message, hashlib.sha256)
+        digest = h.digest()
+        metadata = self.key_salt
+
+        nonce = os.urandom(12)
+        metadata += nonce
+        aesgcm = AESGCM(self.mac_key)
+        ct = aesgcm.encrypt(nonce, message, metadata)
+
+        return digest + nonce + self.key_salt + ct
+
+em = Emitter()
+em.derivate_key()
+#print(str(em.send_message('oiiiiiii'),('utf-8')))
+
+
+
+
   
       
---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import time
+from emitter import Emitter
+from receiver import Receiver
+
+#def read_input():
+    #password = input("Insira a sua password: ")
+    #return password
+
+def main():
+    # Leitura password do emmiter
+    #password = read_input()
+    emmiter = Emitter()
+    # Deriva chave do emitter
+    emmiter.derivate_key()
+    # Emmiter envia dados: key_digest + nonce + salt + mensagem
+    dados = emmiter.send_message("Segredo que não se pode partilhar")
+
+    # print('Texto cifrado:',dados)
+    # Leitura password do receiver
+    #password = read_input()
+    receiver = Receiver()
+    # Deriva chave do receiver
+    receiver.derivate_key(dados)
+    try:
+        # Receiver decifra mensagem
+        receiver.read_message(dados)
+    except:
+        # Falha na autenticação da chave
+        print("Falha na autenticação da chave")  
+    
+
+if __name__ == "__main__":
+    main()
